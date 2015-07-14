074d64afee1c3ec3cd39697d485471fb5e16d7cf
unknown <jngoyi@jngoyi.media5corp.com>
2015-07-14 19:56:15 -0400

    Date picker proper css

--- src/js/modules/datetime/datepicker.controller.js
+++ src/js/modules/datetime/datepicker.controller.js
@@ -8,15 +8,16 @@
 
     angular
         .module('naut')
-        .controller('DatepickerController', DatepickerController);
+        .controller('PeriodDatepickerController', PeriodDatepickerController);
     
-    DatepickerController.$inject = ['$rootScope', '$scope'];
-    function DatepickerController($rootScope, $scope) {
+    /* @ngInject */
+    PeriodDatepickerController.$inject = ['$rootScope', '$scope'];
+    function PeriodDatepickerController($rootScope, $scope) {
+      $scope.openedTo = $scope.openedFrom = false;
 
       $scope.today = function() {
         $scope.dt = new Date();
       };
-
       $scope.today();
 
       $scope.clear = function () {
@@ -29,15 +30,22 @@
       };
 
       $scope.toggleMin = function() {
-        $scope.minDate = $scope.minDate ? null : new Date();
+        $scope.minDate = new Date('2015-06-01T00:00:00.000Z');
       };
       $scope.toggleMin();
 
-      $scope.open = function($event) {
+      $scope.toggleFrom = function($event) {
         $event.preventDefault();
         $event.stopPropagation();
 
-        $scope.opened = true;
+        $scope.openedFrom = !$scope.openedFrom;
+      };
+
+      $scope.toggleTo = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.openedTo = !$scope.openedTo;
       };
 
       $scope.dateOptions = {
@@ -45,8 +53,9 @@
         startingDay: 1
       };
 
-      $scope.initDate = new Date('2016-15-20');
+      $scope.initDate = $scope.today();
       $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
       $scope.format = $scope.formats[0];
+
     }
 })();
