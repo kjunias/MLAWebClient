09478ba484328786933aa77d7b9042474f6312ab
Junias Ngoyi <jngoyi@media5corp.com>
2015-09-17 19:22:09 -0400

    Push for beta

--- src/js/modules/core/constants.js
+++ src/js/modules/core/constants.js
@@ -15,8 +15,8 @@
     .constant('BACKEND', {
       // 'baseURL': 'http://localhost:5555'
       // 'baseURL': 'http://localhost:4242'
-      // 'baseURL': 'http://192.168.38.1:5555'
-      'baseURL': 'http://192.168.40.38:5555'
+      'baseURL': 'http://192.168.38.1:5555'
+      // 'baseURL': 'http://192.168.40.38:5555'
     })
     .constant('MEDIA_QUERY', {
       'desktopLG': 1200,
